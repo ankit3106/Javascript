c0f577e76dd09ade567380472140a4f4d8a5b5fc
Ankit <120703430+ankit3106@users.noreply.github.com>
2024-06-02 18:52:20 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node js, not browser
+
+// various datatypes
+
+// number => 2 to power 53
+// bigint
+// string = ""
+// null=> standalone value ex => let state = null
+// undefined => no value assigned ex => let state;
+// symbol => unique
+
+// object
+
+// console.log(typeof null) // output = object
+// console.log(typeof undefined) // output = undefined
