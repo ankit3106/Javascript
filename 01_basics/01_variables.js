67dbf93cf8677c991f63c6ecbb065b74320947ea
Ankit <120703430+ankit3106@users.noreply.github.com>
2024-06-02 18:25:30 +0000

    change_01

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "luffytarow13@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pithoragarh" // dont use 
+
+// accountId = 2  // not allowed
+ 
+// accountEmail = "ife2022006@iiita.ac.in"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword,accountCity])
