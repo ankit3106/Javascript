c291928cb2036f2a34e96cdae8d3c31bd0388aec
Ankit <120703430+ankit3106@users.noreply.github.com>
2024-06-22 12:30:22 +0000

    loops

---
+++ 05_iterations/forIn.js
@@ -0,0 +1,26 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    // console.log( key, " ", programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) { // map k upar iteration nhi kiya ja sakta
+//     console.log(key);
+// }
